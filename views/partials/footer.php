17c96ffbabc3cc7ec574aadf315c0893ea5ade76
B-asmala <basmalabayoumi@gmail.com>
2025-03-24 20:00:32 +0200

    Add : Footer Partial View

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,3 +1,8 @@
-</body>
-
-</html>
+<footer class="bg-black text-white text-center py-4 font-mono">
+  <p>Developed by:</p>
+  <p>
+    <a href="https://github.com/mohamedadel96e" class="hover:underline">Mohamed Adel</a> &nbsp;&amp;&nbsp;
+    <a href="https://github.com/distru3" class="hover:underline">Hussein Raed</a> &nbsp;&amp;&nbsp;
+    <a href="https://github.com/B-asmala" class="hover:underline">Basmala Abdelhakim</a>
+  </p>
+</footer>
